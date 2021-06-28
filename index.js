fca4fb1f3a8c076b2b097f7cdf5d31f40f7b3081
= <mhasanelmaci@gmail.com>
2021-06-29 02:40:45 +0300

    Add an if statement for production into index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("./src/db/mongoose");
+const path = require("path")
 const cors = require("cors");
 const taskRouter = require("./src/routers/task");
 
@@ -10,6 +11,13 @@ const port = process.env.PORT;
 app.use(express.json());
 
 app.use(taskRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
